f34d86ba47c7544e0d5b46410c276bd5fcfce55b
Paul Monson <paulmon@microsoft.com>
2018-12-18 12:48:55 -0800

    also fix test_distutils

--- Tools/winiot/sync_win_iot.py
+++ Tools/winiot/sync_win_iot.py
@@ -125,35 +125,35 @@ def include_in_tools(p):
 BASE_NAME = 'python??'
 
 FULL_LAYOUT = [
-    ('/', '$source', 'python.exe', is_not_debug, None),
-    ('/', '$source', 'pythonw.exe', is_not_debug, None),
-    ('/', '$source', 'venvlauncher.exe', is_not_debug, 'python.exe'),
-    ('/', '$source', 'venvwlauncher.exe', is_not_debug, 'pythonw.exe'),
-    ('/', '$source', 'python{}.dll'.format(sys.version_info.major), is_not_debug, None),
-    ('/', '$source', '{}.dll'.format(BASE_NAME), is_not_debug, None),
-    ('DLLs/', '$source', '*.pyd', is_not_debug, None),
-    ('DLLs/', '$source', '*.dll', is_not_python, None),
-    ('include/', 'include', '*.h', None, None),
-    ('include/', 'PC', 'pyconfig.h', None, None),
-    ('Lib/', 'Lib', '**/*', include_in_lib, None),
-    ('libs/', '$source', '*.lib', include_in_libs, None),
-    ('Tools/', 'Tools', '**/*', include_in_tools, None),
+    ('/', '$source', 'python.exe', is_not_debug),
+    ('/', '$source', 'pythonw.exe', is_not_debug),
+    ('/', '$source', 'venvlauncher.exe', is_not_debug),
+    ('/', '$source', 'venvwlauncher.exe', is_not_debug),
+    ('/', '$source', 'python{}.dll'.format(sys.version_info.major), is_not_debug),
+    ('/', '$source', '{}.dll'.format(BASE_NAME), is_not_debug),
+    ('DLLs/', '$source', '*.pyd', is_not_debug),
+    ('DLLs/', '$source', '*.dll', is_not_python),
+    ('include/', 'include', '*.h', None),
+    ('include/', 'PC', 'pyconfig.h', None),
+    ('Lib/', 'Lib', '**/*', include_in_lib),
+    ('libs/', '$source', '*.lib', include_in_libs),
+    ('Tools/', 'Tools', '**/*', include_in_tools),
 ]
 
 FULL_LAYOUT_DEBUG = [
-    ('/', '$source', 'python_d.exe', is_debug, None),
-    ('/', '$source', 'pythonw_d.exe', is_debug, None),
-    ('/', '$source', 'venvlauncher_d.exe', is_debug, 'python_d.exe'),
-    ('/', '$source', 'venvwlauncher_d.exe', is_debug, 'pythonw_d.exe'),
-    ('/', '$source', 'python{}_d.dll'.format(sys.version_info.major), is_debug, None),
-    ('/', '$source', '{}_d.dll'.format(BASE_NAME), is_debug, None),
-    ('include/', 'include', '*.h', None, None),
-    ('include/', 'PC', 'pyconfig.h', None, None),
-    ('DLLs/', '$source', '*.pyd', is_debug, None),
-    ('DLLs/', '$source', '*.dll', is_not_python, None),
-    ('Lib/', 'Lib', '**/*', include_in_lib, None),
-    ('libs/', '$source', '*.lib', include_in_libs, None),
-    ('Tools/', 'Tools', '**/*', include_in_tools, None),
+    ('/', '$source', 'python_d.exe', is_debug),
+    ('/', '$source', 'pythonw_d.exe', is_debug),
+    ('Lib/', '$source', 'venvlauncher_d.exe', is_not_debug),
+    ('Lib/', '$source', 'venvwlauncher_d.exe', is_not_debug),
+    ('/', '$source', 'python{}_d.dll'.format(sys.version_info.major), is_debug),
+    ('/', '$source', '{}_d.dll'.format(BASE_NAME), is_debug),
+    ('include/', 'include', '*.h', None),
+    ('include/', 'PC', 'pyconfig.h', None),
+    ('DLLs/', '$source', '*.pyd', is_debug),
+    ('DLLs/', '$source', '*.dll', is_not_python),
+    ('Lib/', 'Lib', '**/*', include_in_lib),
+    ('libs/', '$source', '*.lib', include_in_libs),
+    ('Tools/', 'Tools', '**/*', include_in_tools),
 ]
 
 if os.getenv('DOC_FILENAME'):
@@ -161,7 +161,7 @@ if os.getenv('DOC_FILENAME'):
 if os.getenv('VCREDIST_PATH'):
     FULL_LAYOUT.append(('/', os.getenv('VCREDIST_PATH'), 'vcruntime*.dll', None))
 
-def copy_to_layout(target, rel_sources, target_new_name=None):
+def copy_to_layout(target, rel_sources):
     count = 0
     
     if target.suffix.lower() == '.zip':
@@ -186,11 +186,7 @@ def copy_to_layout(target, rel_sources, target_new_name=None):
     else:
         for s, rel in rel_sources:
             
-            if target_new_name == None:
-                dest = target / rel
-            else:
-                dest = target / target_new_name
-
+            dest = target / rel
             try:
                 dest.parent.mkdir(parents=True)
             except FileExistsError:
@@ -270,17 +266,40 @@ def main():
         print ("full layout selected")
         layout = FULL_LAYOUT
 
-    for t, s, p, c, altname in layout:
+    for t, s, p, c in layout:
         if s == '$source':
             fs = source
         else:
             fs = repo / s
         print('fs = {}'.format(fs))
         files = rglob(fs, p, c, includeDebug)
-        if p.startswith('venv'):
-            copied = copy_to_layout(output / 'lib/venv/scripts/nt', files, altname)
-        else:
-            copied = copy_to_layout(output / t.rstrip('/'), files)
+        extra_files = []
+        if s == 'Lib' and p == '**/*':
+            extra_files.append((
+                repo / 'tools' / 'msi' / 'distutils.command.bdist_wininst.py',
+                Path('distutils') / 'command' / 'bdist_wininst.py'
+            ))
+        if p == 'venvlauncher.exe':
+            extra_files.append((
+                source / p,
+                Path('venv') / 'Scripts' / 'nt' / 'python.exe'
+            ))
+        if p == 'venvwlauncher.exe':
+            extra_files.append((
+                source / p,
+                Path('venv') / 'Scripts' / 'nt' / 'pythonw.exe'
+            ))
+        if p == 'venvlauncher_d.exe':
+            extra_files.append((
+                source / p,
+                Path('venv') / 'Scripts' / 'nt' / 'python_d.exe'
+            ))
+        if p == 'venvwlauncher_d.exe':
+            extra_files.append((
+                source / p,
+                Path('venv') / 'Scripts' / 'nt' / 'pythonw_d.exe'
+            ))
+        copied = copy_to_layout(output / t.rstrip('/'), chain(files, extra_files))
         print('Copied {} files'.format(copied))
 
     print("================================================================================")
